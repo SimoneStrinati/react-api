13178f9a074aea2e860bc79f4d158dd184172d19
simonestrinati <simone.strinati@outlook.it>
2026-01-28 15:22:27 +0100

    test chiamata API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState, useEffect } from 'react'
+import axios from "axios";
+
+const actressApi = "https://lanciweb.github.io/demo/api/actresses/";
 
 
 function App() {
   //const [count, setCount] = useState(0)
 
+axios.get(actressApi).then((res) =>{
+  console.log(res);
+})
+
   return (
     <>
-      
+      <h1>Hello React-API</h1>
     </>
   )
 }

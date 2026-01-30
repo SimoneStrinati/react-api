bd6c44437d59ffc6aed3ee43c5493f5cfc71d00e
simonestrinati <simone.strinati@outlook.it>
2026-01-30 20:49:14 +0100

    esercizio completato

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,38 @@ const actressApi = "https://lanciweb.github.io/demo/api/actresses/";
 
 
 function App() {
-  //const [count, setCount] = useState(0)
+  const [actresses, setActresses] = useState([]);
 
-axios.get(actressApi).then((res) =>{
-  console.log(res);
-})
+  function getData() {
+    axios.get(actressApi).then((res) => {
+      console.log("Dati ricevuti da API", res.data);
+      setActresses(res.data);
+
+
+    })
+  };
+
+  useEffect(getData, []);
+
 
   return (
     <>
       <h1>Hello React-API</h1>
+
+      <div className='flex-container flex-wrap gap'>
+        {actresses.map((actress, i) =>
+
+          <div className="card" key={i}>
+           <h2> {actress.name}</h2>
+            <img className="image"src={actress.image} alt="" />
+            <p>{actress.nationality} {actress.birth_year}</p>
+            <p>Biografia: {actress.biography}</p>
+            <p>Premi: {actress.awards}</p>
+          </div>
+        )}
+
+
+      </div>
     </>
   )
 }
